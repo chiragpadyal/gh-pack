c50ecb937f24c18f0a0122764a90c439d3e70fca
Chirag Padyal <FreakStar03>
2022-12-12 18:21:24 +0530

    feat(view): tabular view of package.json

---
+++ src/components/table.js
@@ -0,0 +1,25 @@
+import { table } from "table";
+import semver from "semver";
+export async function TableView(json) {
+  const data = [["Name", "Version", "Latest Version", "Upto Date", "Health"]];
+  // Map json data to table
+  for (var key in json) {
+    let parseVersion = (val) => (semver.valid(val) ? val : val.substring(1));
+
+    data.push([
+      key, // name
+      json[key].currentVersion, //current version in package.json
+      json[key].latestVersion, //lastest version of package
+      semver.lte(
+        //check if package is uptodate acc. to lastest version
+        parseVersion(json[key].currentVersion),
+        parseVersion(json[key].latestVersion)
+      )
+        ? "yes"
+        : "no",
+      `${json[key].score}%`, //health of package acc. to npm server
+    ]);
+  }
+
+  console.log(table(data)); //print table
+}
