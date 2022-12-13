cc6fbacc9d7cf5abc0d7a49b51dc7196e7114cc6
chiragpadyal <freakstar03@gmail.com>
2022-12-13 20:39:21 +0530

    fix: file naming

---
+++ src/components/checkNpmServer.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export async function checkNpmServer(json) {
+  let dict = {};
+  for (var key in json) {
+    await axios
+      .get(`https://registry.npmjs.com/-/v1/search?text=${key}&size=1`)
+      .then(function (response) {
+        let version = response.data.objects[0].package.version;
+        let score = response.data.objects[0].score.final;
+        // console.log(response.data.objects[0].package.version);
+        dict[key] = {
+          currentVersion: json[key],
+          latestVersion: version,
+          score: (score * 100) | 0,
+        };
+      })
+      .catch((err) => {
+        dict = {};
+        new Error(err);
+      });
+  }
+  return new Promise(function (myResolve, myReject) {
+    if (dict == {}) myReject(new Error("unable to fetch data"));
+    myResolve(dict);
+  });
+}
