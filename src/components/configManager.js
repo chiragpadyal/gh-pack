11c90a3f17f0061c4422bec2bdc9b54a2c5d39b0
chiragpadyal <freakstar03@gmail.com>
2022-12-13 20:40:00 +0530

    feat: added store, to store user config

---
+++ src/components/configManager.js
@@ -0,0 +1,9 @@
+import { Store } from "data-store";
+import { cwd } from "process";
+import { join } from "path";
+import * as os from "os";
+const store = new Store({
+  home: join(cwd(), ".config/npm-gui.config"),
+  name: "settings",
+});
+export default store;
