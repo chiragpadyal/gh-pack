c5eacc06939cb281acbaa3612380e1cd9b3faa91
chiragpadyal <freakstar03@gmail.com>
2022-12-14 17:44:47 +0530

    fix: fix minor bugs

--- src/index.js
+++ src/index.js
@@ -8,24 +8,41 @@ import { ghUrlSeparate } from "./components/ghUrlSep.js";
 
 let program = new Command();
 
-async function bump(githubUrl, options) {
-  let ghData = ghUrlSeparate(githubUrl);
-  let data = {
-    username: ghData.username,
-    repo: ghData.repo,
-    force: options.force ? true : false,
-    packageName: options.packageName,
-    branchName: options.defaultBranch,
-    version: options.version,
-  };
-
-  bumpVersion(data);
+async function bump(options) {
+  let packageName,
+    version = "";
+  let changedPackage = [];
+  let ghData = ghUrlSeparate(options.githubUrl);
+  options.packageVersion.map((value) => {
+    let splitedName = value.split("@");
+    if (splitedName[1]) {
+      packageName = splitedName[0];
+      version = splitedName[1];
+    }
+    let data = {
+      username: ghData.username,
+      repo: ghData.repo,
+      force: options.force ? true : false,
+      packageName: packageName,
+      branchName: options.defaultBranch,
+      version: version,
+    };
+    bumpVersion(data)
+      .then((data) =>
+        console.log(`Done! Bumped ${packageName} to version ${version}`)
+      )
+      .catch((err) =>
+        console.log(`failed! To bump ${packageName} to version ${version}`)
+      );
+  });
 }
 
 console.log(
   gradient("red", "green", "blue")(figlet.textSync("N P M  G U I !"))
 );
-
+function collect(value, previous) {
+  return previous.concat([value]);
+}
 program
   .name("npm-gui")
   .description("CLI to view package.json and bump dependencies version.")
@@ -50,18 +67,22 @@ program
 program
   .command("bump")
   .description("Bump a package version on github repository!")
-  .option("-p, --package-name <string>", "package name", "all")
-  .option("-df, --default-branch <string>", "default branch name", "main")
+  .option("-df, --default-branch <string>", "main branch name", "master")
   .option(
     "--force",
     "force bump version, even if logged-in username and target repo usernmae are different"
   )
-  .option(
-    "-v, --version <string>",
-    "version of pakage default: latest",
-    "latest"
+  .requiredOption(
+    "-pv , --package-version <package@version>",
+    "list of package@version i.e -pv react@1.0.0 -pv redux@latest -pv react-dom@1.0.0 .... \
+     If the -pv value is all (i.e -pv all) than all packages will be bump!",
+    (value, previous) => previous.concat([value]),
+    []
   )
-  .argument("<github-url>", "github repository http url")
+  .requiredOption(
+    "-gh, --github-url <github-url>",
+    "github repository http url"
+  )
   .action(bump);
 
 program.parse();
